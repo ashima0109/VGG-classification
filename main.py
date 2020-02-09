52344a029e853c473396581fcea0160b6a84c811
Ashima <ashima0109@gmail.com>
2020-02-09 14:19:04 +0800

    Added prediction script and played with hyperparas

---
+++ main.py
@@ -0,0 +1,35 @@
+
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+import os
+
+main_dir = os.getcwd()
+art_dir = os.path.join(main_dir, 'artifact')
+
+# construct the argument parse and parse the arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-train", action='store_true',
+	help="trains the model")
+parser.add_argument("-predict", "--predict", required=False,
+	help="path to file")
+
+args = vars(parser.parse_args())
+
+# for training
+if args['train']:
+	import train
+	train.train_model()
+
+# for prediction
+if args['predict']:
+	image_path = args['predict']
+
+	# check for a valid image path
+	if os.path.isfile(image_path):
+		import predict
+		predict.make_prediction(image_path, art_dir)
+	else:
+		print('Please provide a valid image path')
---
+++ predict.py
@@ -0,0 +1,72 @@
+
+# import the necessary packages
+from keras.models import load_model
+import argparse
+import pickle
+import cv2
+from keras.preprocessing.image import img_to_array
+import numpy as np
+import logging, os
+
+# disable the warnings
+logging.disable(logging.WARNING)
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+
+# function to process the image similar to the training process
+def process_image(image_path):
+
+	# load the input image and resize it to the target spatial dimensions
+	image = cv2.imread(image_path)
+	width, height = 32,32
+
+	image = cv2.resize(image, (width, height))
+
+	# split the channel
+	b, _ , _ = cv2.split(image)
+
+	# image to array
+	b = img_to_array(b)
+
+	# normalize the image
+	processed_image = np.array(b, dtype="float") / 255.0
+
+	return processed_image
+
+
+def make_prediction(image_path, model_dir):
+
+	# process the image
+	image = process_image(image_path)
+
+	# read the image for the output
+	output = cv2.imread(image_path)
+
+	# load the model and label binarizer from the directory
+	print("[INFO] loading model and label encoder...")
+
+	# relative paths to the model and labels
+	model_path = os.path.join(model_dir, 'path_to_my_model.h5')
+	label_file_path = os.path.join(model_dir, 'labels')
+
+	# load the model and the label encoder
+	model = load_model(model_path)
+	lb = pickle.loads(open(label_file_path, "rb").read())
+
+	# make a prediction on the image
+	image = np.expand_dims(image, axis = 0)
+	preds = model.predict(image,  batch_size = 1)
+
+	# extract the class label which has the highest corresponding probability
+	i = preds.argmax(axis=1)[0]
+	label = lb.classes_[i]
+
+	# draw the class label + probability on the output image
+	text = "{}: {:.2f}%".format(label, preds[0][i] * 100)
+	cv2.putText(output, text, (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+	# display the result on the screen
+	print("Predicted label {}: {:.2f}%".format(label, preds[0][i] * 100))
+
+	# save the output image with label
+	cv2.imwrite('output.jpg', output)
